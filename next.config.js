71bc6c1d12cb56c20e898abeeeeb578289fdc83a
Juanma Durand <juanma.durand@gmail.com>
2024-11-04 11:45:18 +0100

    Upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+    silenceDeprecations: ['legacy-js-api'],
   },
-}
+};
 
 module.exports = nextConfig;
